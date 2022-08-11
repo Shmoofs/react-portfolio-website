cb26f47904d7f3ff700b9c627e63feba47fd1edf
Shmoofs <timothyalsaudi@gmail.com>
2022-08-10 22:38:01 -0700

    Front end hell

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-/*import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import Layout from './components/Layout'
-import Portfolio from './components/Portfolio'
-import Dashboard from './components/Dashboar'
-*/
-import About from './components/About'
-import Contact from './components/Contact'
-import Navbar from './components/Navbar'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-import Testimonials from './components/Testimonials'
+import About from './components/About.js'
+import Contact from './components/Contact.js'
+import Navbar from './components/Navbar.js'
+import Projects from './components/Projects.js'
+import Skills from './components/Skills.js'
+import Testimonials from './components/Testimonials.js'
 
 import './App.scss'
 import React from "react"
-
-/* function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </>
-  )
-} */
 
 export default function App(){
   return (
